5279e0889268d98186ddbc2f32247cdf22d02d3b
Vikraman Choudhury <git@vikraman.org>
2016-09-11 18:52:00 -0400

    Export Ord methods from VerifiedOrd

--- src/Data/VerifiedOrd.hs
+++ src/Data/VerifiedOrd.hs
@@ -1,12 +1,23 @@
 {-@ LIQUID "--higherorder"     @-}
 {-@ LIQUID "--totality"        @-}
 
-module Data.VerifiedOrd (VerifiedOrd, leqOrd, cmpOrd, (==)) where
+module Data.VerifiedOrd (
+    VerifiedOrd,
+    leqOrd,
+    cmpOrd,
+    (<),
+    (<=),
+    (>),
+    (>=),
+    max,
+    min,
+    ) where
 
-import Prelude hiding ((==))
+import Prelude hiding ((<), (<=), (>), (>=))
 import Language.Haskell.Liquid.ProofCombinators
 
-data VerifiedOrd a = LEq (a -> a -> Bool) | Cmp (a -> a -> Ordering)
+data VerifiedOrd a = LEq (a -> a -> Bool)
+                   | Cmp (a -> a -> Ordering)
 
 {-@
 leqOrd :: leq:(a -> a -> Bool)
@@ -26,6 +37,24 @@ leqOrd leq total antisym trans = LEq leq
 cmpOrd :: (a -> a -> Ordering) -> VerifiedOrd a
 cmpOrd cmp = undefined
 
-(==) :: VerifiedOrd a -> a -> a -> Bool
-(==) (LEq f) x y = f x y
-(==) (Cmp f) x y = case f x y of { GT -> False ; _ -> True }
+(<) :: Eq a => VerifiedOrd a -> a -> a -> Bool
+(<) (LEq f) x y = f x y && not (x == y)
+(<) (Cmp f) x y = case f x y of { LT -> True ; _ -> False }
+
+(<=) :: VerifiedOrd a -> a -> a -> Bool
+(<=) (LEq f) x y = f x y
+(<=) (Cmp f) x y = case f x y of { GT -> False ; _ -> True }
+
+(>) :: VerifiedOrd a -> a -> a -> Bool
+(>) (LEq f) x y = not (f x y)
+(>) (Cmp f) x y = case f x y of { GT -> True ; _ -> False }
+
+(>=) :: Eq a => VerifiedOrd a -> a -> a -> Bool
+(>=) (LEq f) x y = not (f x y) || x == y
+(>=) (Cmp f) x y = case f x y of { LT -> False ; _ -> False }
+
+max :: VerifiedOrd a -> a -> a -> a
+max v x y = if ((<=) v x y) then y else x
+
+min :: VerifiedOrd a -> a -> a -> a
+min v x y = if ((<=) v x y) then x else y
