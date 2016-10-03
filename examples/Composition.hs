6cc8064f9fc19a85dc1074f703e8a65f0801bea5
Vikraman Choudhury <git@vikraman.org>
2016-10-03 03:49:40 -0400

    Example for proof composition

---
+++ examples/Composition.hs
@@ -0,0 +1,22 @@
+{-@ LIQUID "--totality"       @-}
+{-@ LIQUID "--higherorder"    @-}
+
+module Composition where
+
+import Data.VerifiedEq
+import Data.VerifiableConstraint
+import Language.Haskell.Liquid.ProofCombinators
+
+{-@ assume veqInt :: VerifiedEq Int @-}
+veqInt :: VerifiedEq Int
+veqInt = VerifiedEq (==) undefined undefined undefined
+
+{-@ assume veqString :: VerifiedEq String @-}
+veqString :: VerifiedEq String
+veqString = VerifiedEq (==) undefined undefined undefined
+
+-- This is a bad thing
+x :: Bool
+x = using (VEq veqInt)
+  $ using (VEq veqString)
+  $ (1, "foo") /= (2, "bar")
