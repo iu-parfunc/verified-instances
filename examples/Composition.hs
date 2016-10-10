14a73ac437bd09f0dc0bfb7374bb9c828d02c24f
Vikraman Choudhury <git@vikraman.org>
2016-10-10 01:37:49 -0400

    Update example for products

--- examples/Composition.hs
+++ examples/Composition.hs
@@ -4,24 +4,26 @@
 module Composition where
 
 import Data.VerifiedEq
+import Data.VerifiedEq.Instances
 import Data.VerifiableConstraint
 import Language.Haskell.Liquid.ProofCombinators
-
-{-@ assume veqInt :: VerifiedEq Int @-}
-veqInt :: VerifiedEq Int
-veqInt = VerifiedEq (==) undefined undefined undefined
 
 {-@ assume veqString :: VerifiedEq String @-}
 veqString :: VerifiedEq String
 veqString = VerifiedEq (==) undefined undefined undefined
 
--- This is a bad thing
+-- bad usage
 x :: Bool
 x = using (VEq veqInt)
   $ using (VEq veqString)
   $ ('1', "foo") /= ('2', "bar")
 
+-- good usage
 y :: Bool
-y = using (VEq veqInt)
+y = using (VEq $ veqProd veqInt veqString)
+  $ ('1', "foo") /= ('2', "bar")
+
+z :: Bool
+z = using (VEq veqInt)
   $ using (VEq veqString)
   $ Left '1' /= Right "foo"
