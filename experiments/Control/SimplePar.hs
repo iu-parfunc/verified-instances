91d8b163d062ce90a5ce3342d597cfd043193e8f
Vikraman Choudhury <git@vikraman.org>
2017-01-08 19:55:34 -0500

    Add quickcheck test for the main theorem, but it fails

--- experiments/Control/SimplePar.hs
+++ experiments/Control/SimplePar.hs
@@ -1,22 +1,30 @@
-{-@ LIQUID "--higherorder" @-}
-{-# LANGUAGE BangPatterns #-}
+{-@ LIQUID "--higherorder"        @-}
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 #!/usr/bin/env stack
 -- stack --resolver lts-6.20 --install-ghc runghc --package containers
 
 -- |
 
-module Control.SimplePar
+module Main
     ( new,put,get
     , IVar, Par
     , main, err, deadlock, loop, dag
     )
     where
 
+import GHC.Generics
 import Control.Monad
 import Control.Monad.Except
 import Data.IntMap as M hiding (fromList)
 import Data.List as L
-
+import Test.QuickCheck
+import Test.QuickCheck.Modifiers
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
 
 --------------------------------------------------------------------------------
 
@@ -36,7 +44,17 @@ data Trace = Get (IVar Val) (Val -> Trace)
            | New (IVar Val -> Trace)
            | Fork Trace Trace
            | Done
+  deriving (Generic, CoArbitrary)
 
+instance Arbitrary Trace where
+  arbitrary =
+    oneof
+      [ return Done
+      , liftM2 Fork arbitrary arbitrary
+      , liftM New arbitrary
+      , liftM3 Put arbitrary arbitrary arbitrary -- (arbitrary `suchThat` (>= 0))
+      , liftM2 Get arbitrary arbitrary
+      ]
 
 -- | Hack: hardcode all IVar values to Double:
 type Val = Double
@@ -45,6 +63,12 @@ type Val = Double
 
 -- | An IVar is an index into an IntMap
 data IVar a = IVar Int
+
+instance Arbitrary (IVar a) where
+  arbitrary = liftM IVar arbitrary -- (arbitrary `suchThat` (>= 0))
+
+instance CoArbitrary (IVar a) where
+  coarbitrary (IVar v) = coarbitrary v
 
 type Heap = M.IntMap (Maybe Val)
 
@@ -72,6 +96,12 @@ newtype Par a = Par {
     runCont :: (a -> Trace) -> Trace
 }
 
+instance Show (Par a) where
+  show _ = "Par"
+
+instance Arbitrary (Par Val) where
+  arbitrary = liftM Par arbitrary
+
 instance Functor Par where
     fmap f m = Par $ \c -> runCont m (c . f)
 
@@ -87,9 +117,21 @@ instance Monad Par where
 
 data InfList a = Cons a (InfList a)
 
+instance Show a => Show (InfList a) where
+  show xs = show (take 10 (toList' xs)) ++ "..."
+
+instance Functor InfList where
+  fmap f (Cons a as) = Cons (f a) (fmap f as)
+
 fromList :: [a] -> InfList a
 fromList (a:b) = Cons a (fromList b)
 fromList [] = error "fromList: cannot convert finite list to infinite list!"
+
+toList' :: InfList a -> [a]
+toList' (Cons a as) = a : toList' as
+
+instance Arbitrary a => Arbitrary (InfList a) where
+  arbitrary = liftM fromList infiniteList
 
 --------------------------------------------------------------------------------
 -- The scheduler itself
@@ -101,6 +143,13 @@ fromList [] = error "fromList: cannot convert finite list to infinite list!"
 -- | Exception thrown by runPar
 data Exn = MultiplePut Val Int Val
          | Deadlock (M.IntMap [Val -> Trace])
+         | HeapExn
+
+instance Eq Exn where
+  MultiplePut _ _ _ == MultiplePut _ _ _ = True
+  Deadlock _ == Deadlock _ = True
+  HeapExn == HeapExn = True
+  _ == _ = False
 
 instance Show Exn where
   show (MultiplePut v ix v0) =
@@ -109,6 +158,8 @@ instance Show Exn where
   show (Deadlock blkd) =
     "no runnable threads, but " ++ show (sum (L.map length (M.elems blkd))) ++
     " thread(s) blocked on these IVars: " ++ show (M.keys blkd)
+  show HeapExn =
+    "heap location 0 was empty"
 
 -- we can syntactically describe parallel evaluation contexts if we like:
 --   fork (a1 >>= k1) (a2 >>= k2)
@@ -127,8 +178,10 @@ runPar randoms p = do
       initThreads = [runCont p (\v -> Put (IVar 0) v Done)]
 
   finalHeap <- sched randoms initThreads M.empty 1 initHeap
-  let Just finalVal = finalHeap M.! 0
-  return finalVal
+  let maybeFinalVal = finalHeap M.! 0
+  case maybeFinalVal of
+    Nothing       -> throwError HeapExn
+    Just finalVal -> return finalVal
 
   where
     sched :: InfList Word -> [Trace] -> M.IntMap [Val -> Trace] -> Int -> Heap -> Except Exn Heap
@@ -148,12 +201,12 @@ runPar randoms p = do
         Fork t1 t2 -> return (t1 : t2 : others, blkd, cntr, heap)
         New k -> return (k (IVar cntr) : others, blkd, cntr + 1, M.insert cntr Nothing heap)
         Get (IVar ix) k ->
-          case heap M.! ix of
+          case join (M.lookup ix heap) of
             Nothing -> return (others, M.insertWith (++) ix [k] blkd, cntr, heap)
             Just v  -> return (k v : others, blkd, cntr, heap)
         Put (IVar ix) v t2 ->
           let heap' = M.insert ix (Just v) heap
-          in case heap M.! ix of
+          in case join (M.lookup ix heap) of
             Nothing ->
               case M.lookup ix blkd of
                 Nothing -> return (t2 : others, blkd, cntr, heap')
@@ -161,22 +214,32 @@ runPar randoms p = do
                                  , M.delete ix blkd, cntr, heap')
             Just v0 -> throwError $ MultiplePut v ix v0
 
-    yank n ls = let (hd,x:tl) = splitAt (fromIntegral n `mod` length ls) ls
-                in (x, hd++tl)
+    yank n ls =
+      let (hd, x:tl) = splitAt (fromIntegral n `mod` length ls) ls
+      in (x, hd ++ tl)
+
+runPar' :: InfList Word -> Par Val -> Either Exn Val
+runPar' randoms p = runExcept (runPar randoms p)
 
 --------------------------------------------------------------------------------
 
 roundRobin :: InfList Word
 roundRobin = fromList [0..]
 
+canonical :: InfList Word
+canonical = fromList (repeat 0)
+
 main :: IO ()
-main = do
-  print $ runPar roundRobin (return 3.99)
+main = defaultMain tests
 
-  print $ runPar roundRobin (do v <- new; put v 3.12; get v)
+  -- print $ runPar' roundRobin (return 3.99)
+  -- print $ runPar' roundRobin (do v <- new; put v 3.12; get v)
 
--- TODO: make this into a quickcheck test harness.
-
+tests = testGroup "Tests"
+          [ QC.testProperty "forall p l1 . runPar l1 p == runPar (repeat 0) p" $
+            \p (l1 :: InfList (NonNegative Word)) ->
+              runPar' (getNonNegative <$> l1) p == runPar' canonical p
+          ]
 
 -- | Example error
 err :: IO ()
