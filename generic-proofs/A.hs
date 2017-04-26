cf04667e99d9724ad0e1231bdb8934b9a5655939
Ryan Scott <ryan.gl.scott@gmail.com>
2017-04-26 16:04:59 -0400

    Reduced example

---
+++ generic-proofs/A.hs
@@ -0,0 +1,21 @@
+-- A.hs
+{-@ LIQUID "--higherorder"        @-}
+{-@ LIQUID "--totality"           @-}
+{-@ LIQUID "--exactdc"            @-}
+module A where
+
+import K1
+import Language.Haskell.Liquid.ProofCombinators
+
+{-@ axiomatize eqK1 @-}
+eqK1 :: (c -> c -> Bool) -> K1 i c p -> K1 i c p -> Bool
+eqK1 eqC x y = eqC (unK1 x) (unK1 y)
+
+{-@ eqK1Refl :: eqC:(c -> c -> Bool) -> eqCRefl:(x:c -> { eqC x x })
+             -> x:K1 i c p -> { eqK1 eqC x x } @-}
+eqK1Refl :: (c -> c -> Bool) -> (c -> Proof) -> K1 i c p -> Proof
+eqK1Refl eqC eqCRefl x
+  =   eqK1 eqC x x
+  ==. eqC (unK1 x) (unK1 x)
+  ==. True ? eqCRefl (unK1 x)
+  *** QED
