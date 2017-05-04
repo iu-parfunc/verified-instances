8a422d11afe8aca806bb2f826e4a328fd99c61dd
Ryan Scott <ryan.gl.scott@gmail.com>
2017-05-04 09:15:37 -0400

    Minor refactoring

--- generic-proofs/src/GenericProofs/TH.hs
+++ generic-proofs/src/GenericProofs/TH.hs
@@ -9,25 +9,27 @@ import Language.Haskell.Liquid.ProofCombinators
 import Language.Haskell.TH
 import Language.Haskell.TH.Lens (typeVars)
 
-deriveIso :: String -- ^ Name of the proof that @to . from = id@
+deriveIso :: String -- ^ Name of the @to@ function
+          -> String -- ^ Name of the @from@ function
+          -> String -- ^ Name of the proof that @to . from = id@
           -> String -- ^ Name of the proof that @from . to = id@
           -> String -- ^ Name of the 'Iso'
           -> Name   -- ^ Name of the datatype
           -> Q [Dec]
-deriveIso tof fot iso n = do
+deriveIso toFun fromFun tof fot iso n = do
   metaDecs        <- deriveMeta n
   genericInstance <- deriveRepresentable0 n
   fromExp         <- makeFrom0 n
   toExp           <- makeTo0 n
   x               <- newName "x"
   z               <- newName "z"
-  fromInternal    <- newName "fromInternal"
-  toInternal      <- newName "toInternal"
 
-  let tofN, fotN, isoN :: Name
-      tofN = mkName tof
-      fotN = mkName fot
-      isoN = mkName iso
+  let fromN, toN, tofN, fotN, isoN :: Name
+      fromN = mkName fromFun
+      toN   = mkName toFun
+      tofN  = mkName tof
+      fotN  = mkName fot
+      isoN  = mkName iso
 
       -- Technically, we could also retrieve this information from
       -- deriveRepresentable0, but this is a smidge more convenient.
@@ -47,10 +49,13 @@ deriveIso tof fot iso n = do
                    _ -> fail "deriveIso: dataType"
       repType = [t| Rep $(dataType) $(varT x) |]
 
+      mkForallT :: Type -> Q Type -> Q Type
+      mkForallT ty qBodyTy = forallT (map PlainTV (ty^..typeVars)) (return []) qBodyTy
+
       isoDecsQ :: Q [Dec]
       isoDecsQ = do
         ty <- repType
-        sequence [ sigD isoN $ forallT (map PlainTV (ty^..typeVars)) (return []) [t| Iso $(dataType) $(repType) |]
+        sequence [ sigD isoN $ mkForallT ty [t| Iso $(dataType) $(repType) |]
                  , funD isoN [clause [] (normalB [| Iso from to $(varE fotN) $(varE tofN) |]) []]
                  ]
 
@@ -58,19 +63,19 @@ deriveIso tof fot iso n = do
       internalDecsQ internalN funN ty1 ty2 = do
         rt <- repType
         sequence
-          [ sigD internalN $ forallT (map PlainTV (rt^..typeVars)) (return []) [t| $(ty1) -> $(ty2) |]
+          [ sigD internalN $ mkForallT rt [t| $(ty1) -> $(ty2) |]
           , funD internalN [clause [] (normalB (varE funN)) []]
           ]
 
       fromInternalDecsQ, toInternalDecsQ :: Q [Dec]
-      fromInternalDecsQ = internalDecsQ fromInternal 'from dataType repType
-      toInternalDecsQ   = internalDecsQ toInternal   'to   repType  dataType
+      fromInternalDecsQ = internalDecsQ fromN 'from dataType repType
+      toInternalDecsQ   = internalDecsQ toN   'to   repType  dataType
 
       proofDecsQ :: Name -> Name -> Name -> Q Type -> [(Pat,Exp)] -> Q [Dec]
       proofDecsQ fun1oFun2N fun1N fun2N qTy patsAndExps = do
         ty <- qTy
         sequence
-          [ sigD fun1oFun2N $ forallT (map PlainTV (ty^..typeVars)) (return []) [t| $(qTy) -> Proof |]
+          [ sigD fun1oFun2N $ mkForallT ty [t| $(qTy) -> Proof |]
           , funD fun1oFun2N $ map (\(pat,expr) -> clause [asP z $ return pat]
                                                          (normalB [|     $(varE fun1N) ($(varE fun2N) $(varE z))
                                                                      ==. $(varE fun1N) $(return expr)
@@ -81,8 +86,8 @@ deriveIso tof fot iso n = do
           ]
 
       fotDecsQ, tofDecsQ :: Q [Dec]
-      fotDecsQ = proofDecsQ fotN fromInternal toInternal repType  fotPatsAndExps
-      tofDecsQ = proofDecsQ tofN toInternal fromInternal dataType tofPatsAndExps
+      fotDecsQ = proofDecsQ fotN fromN toN repType  fotPatsAndExps
+      tofDecsQ = proofDecsQ tofN toN fromN dataType tofPatsAndExps
 
   fromInternalDecs <- fromInternalDecsQ
   toInternalDecs   <- toInternalDecsQ
