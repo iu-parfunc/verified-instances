a10359e3fdce457816f2a581e4cfd64a6ac378e7
Vikraman Choudhury <git@vikraman.org>
2017-05-20 02:18:50 -0400

    VerifiedFunctor example for List

---
+++ generic-proofs/src/GenericProofs/VerifiedFunctor/Examples/List.hs
@@ -0,0 +1,67 @@
+{-@ LIQUID "--higherorder"        @-}
+{-@ LIQUID "--totality"           @-}
+{-@ LIQUID "--exactdc"            @-}
+{-# LANGUAGE Rank2Types #-}
+
+module GenericProofs.VerifiedFunctor.Examples.List where
+
+import Language.Haskell.Liquid.ProofCombinators
+
+import GenericProofs.Iso
+import GenericProofs.VerifiedFunctor
+import GenericProofs.VerifiedFunctor.Generics
+
+import Generics.Deriving.Newtypeless.Base.Internal
+
+{-@ data List a = Nil | Cons { hd :: a, tl :: List a } @-}
+data List a = Nil | Cons { hd :: a, tl :: List a }
+
+type RepList = Sum U1 (Product Par1 (Rec1 List))
+
+{-@ axiomatize fromList @-}
+fromList :: forall a. List a -> RepList a
+fromList Nil         = L1 U1
+fromList (Cons x xs) = R1 (Product (Par1 x) (Rec1 xs))
+
+{-@ axiomatize toList @-}
+toList :: forall a. RepList a -> List a
+toList (L1 U1)                           = Nil
+toList (R1 (Product (Par1 x) (Rec1 xs))) = Cons x xs
+
+{-@ tofList :: forall a. l:List a -> { toList (fromList l) == l } @-}
+tofList :: forall a. List a -> Proof
+tofList Nil
+  =   toList (fromList Nil)
+  ==. toList (L1 U1)
+  ==. Nil
+  *** QED
+tofList (Cons x xs)
+  =   toList (fromList (Cons x xs))
+  ==. toList (R1 (Product (Par1 x) (Rec1 xs)))
+  ==. Cons x xs
+  *** QED
+
+{-@ fotList :: forall a. l:RepList a -> { fromList (toList l) == l } @-}
+fotList :: forall a. RepList a -> Proof
+fotList (L1 U1)
+  =   fromList (toList (L1 U1))
+  ==. fromList Nil
+  ==. L1 U1
+  *** QED
+fotList (R1 (Product (Par1 x) (Rec1 xs)))
+  =   fromList (toList (R1 (Product (Par1 x) (Rec1 xs))))
+  ==. fromList (Cons x xs)
+  ==. R1 (Product (Par1 x) (Rec1 xs))
+  *** QED
+
+isoList :: Iso1 RepList List
+isoList = Iso1 toList fromList tofList fotList
+
+{-@ lazy vfunctorList @-}
+vfunctorList :: VerifiedFunctor List
+vfunctorList =
+    vfunctorIso
+        isoList
+        (vfunctorSum
+             vfunctorU1
+             (vfunctorProduct vfunctorPar1 (vfunctorRec1 vfunctorList)))
