a795073bd75b833fbce0e9ade4aa5f6b4bf3d7db
Ryan Scott <ryan.gl.scott@gmail.com>
2017-05-20 19:05:58 -0400

    VerifiedSemigroup instances for Identity, Triple

---
+++ generic-proofs/src/GenericProofs/VerifiedSemigroup/Examples/Identity.hs
@@ -0,0 +1,46 @@
+{-@ LIQUID "--higherorder"        @-}
+{-@ LIQUID "--totality"           @-}
+{-@ LIQUID "--exactdc"            @-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+module GenericProofs.VerifiedSemigroup.Examples.Identity
+  ( Identity(..)
+  , RepIdentity
+  , toIdentity
+  , fromIdentity
+  , tofIdentity
+  , fotIdentity
+  , isoIdentity
+  , vsemigroupIdentity
+  ) where
+
+import Language.Haskell.Liquid.ProofCombinators
+
+import GenericProofs.Iso
+import GenericProofs.TH
+import GenericProofs.VerifiedSemigroup
+import GenericProofs.VerifiedSemigroup.Generics
+import GenericProofs.VerifiedSemigroup.Instances
+
+import Generics.Deriving.Newtypeless.Base.Internal
+
+-- Morally a newtype, but in practice, not.
+{-@ data Identity a = Identity { getIdentity :: a } @-}
+data Identity a = Identity { getIdentity :: a }
+
+{-@ axiomatize fromIdentity @-}
+{-@ axiomatize toIdentity @-}
+{-@ tofIdentity :: a:Identity
+                -> { toIdentity (fromIdentity a) == a }
+@-}
+{-@ fotIdentity :: a:RepIdentity a x
+                -> { fromIdentity (toIdentity a) == a }
+@-}
+$(deriveIso "RepIdentity"
+            "toIdentity" "fromIdentity"
+            "tofIdentity" "fotIdentity"
+            "isoIdentity"
+            ''Identity)
+
+vsemigroupIdentity :: VerifiedSemigroup (Identity Unit)
+vsemigroupIdentity = vsemigroupIso (isoSym isoIdentity) $ vsemigroupM1 $ vsemigroupM1 $ vsemigroupM1 $ vsemigroupK1 vsemigroupUnit
---
+++ generic-proofs/src/GenericProofs/VerifiedSemigroup/Examples/Triple.hs
@@ -0,0 +1,45 @@
+{-@ LIQUID "--higherorder"        @-}
+{-@ LIQUID "--totality"           @-}
+{-@ LIQUID "--exactdc"            @-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+module GenericProofs.VerifiedSemigroup.Examples.Triple where
+
+
+import Language.Haskell.Liquid.ProofCombinators
+
+import GenericProofs.Iso
+import GenericProofs.TH
+import GenericProofs.VerifiedSemigroup
+import GenericProofs.VerifiedSemigroup.Generics
+import GenericProofs.VerifiedSemigroup.Instances
+
+import Generics.Deriving.Newtypeless.Base.Internal
+
+
+{-@ data Triple a b c = MkTriple { fld1 :: a, fld2 :: b, fld3 :: c } @-}
+data Triple a b c = MkTriple a b c
+
+{-@ axiomatize fromTriple @-}
+{-@ axiomatize toTriple @-}
+{-@ tofTriple :: a:Triple a b c
+              -> { toTriple (fromTriple a) == a }
+@-}
+{-@ fotTriple :: a:RepTriple a b c x
+              -> { fromTriple (toTriple a) == a }
+@-}
+$(deriveIso "RepTriple"
+            "toTriple" "fromTriple"
+            "tofTriple" "fotTriple"
+            "isoTriple"
+            ''Triple)
+
+vsemigroupTriple :: VerifiedSemigroup (Triple Unit Unit Unit)
+vsemigroupTriple =
+    vsemigroupIso (isoSym isoTriple)
+               $ vsemigroupM1
+               $ vsemigroupM1
+               $ vsemigroupProd (vsemigroupM1 $ vsemigroupK1 vsemigroupUnit)
+               $ vsemigroupProd (vsemigroupM1 $ vsemigroupK1 vsemigroupUnit)
+                         (vsemigroupM1 $ vsemigroupK1 vsemigroupUnit)
+
