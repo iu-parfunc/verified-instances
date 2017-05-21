8b1a16307d5b4596a9fb1707c87d776d84a6aade
Ryan Scott <ryan.gl.scott@gmail.com>
2017-05-20 20:30:30 -0400

    VerifiedMonoid for Identity, Triple

---
+++ generic-proofs/src/GenericProofs/VerifiedMonoid/Examples/Identity.hs
@@ -0,0 +1,46 @@
+{-@ LIQUID "--higherorder"        @-}
+{-@ LIQUID "--totality"           @-}
+{-@ LIQUID "--exactdc"            @-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+module GenericProofs.VerifiedMonoid.Examples.Identity
+  ( Identity(..)
+  , RepIdentity
+  , toIdentity
+  , fromIdentity
+  , tofIdentity
+  , fotIdentity
+  , isoIdentity
+  , vmonoidIdentity
+  ) where
+
+import Language.Haskell.Liquid.ProofCombinators
+
+import GenericProofs.Iso
+import GenericProofs.TH
+import GenericProofs.VerifiedMonoid
+import GenericProofs.VerifiedMonoid.Generics
+import GenericProofs.VerifiedMonoid.Instances
+
+import Generics.Deriving.Newtypeless.Base.Internal
+
+-- Morally a newtype, but in practice, not.
+{-@ data Identity a = Identity { getIdentity :: a } @-}
+data Identity a = Identity { getIdentity :: a }
+
+{-@ axiomatize fromIdentity @-}
+{-@ axiomatize toIdentity @-}
+{-@ tofIdentity :: a:Identity
+                -> { toIdentity (fromIdentity a) == a }
+@-}
+{-@ fotIdentity :: a:RepIdentity a x
+                -> { fromIdentity (toIdentity a) == a }
+@-}
+$(deriveIso "RepIdentity"
+            "toIdentity" "fromIdentity"
+            "tofIdentity" "fotIdentity"
+            "isoIdentity"
+            ''Identity)
+
+vmonoidIdentity :: VerifiedMonoid (Identity Unit)
+vmonoidIdentity = vmonoidIso (isoSym isoIdentity) $ vmonoidM1 $ vmonoidM1 $ vmonoidM1 $ vmonoidK1 vmonoidUnit
---
+++ generic-proofs/src/GenericProofs/VerifiedMonoid/Examples/Triple.hs
@@ -0,0 +1,45 @@
+{-@ LIQUID "--higherorder"        @-}
+{-@ LIQUID "--totality"           @-}
+{-@ LIQUID "--exactdc"            @-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+module GenericProofs.VerifiedMonoid.Examples.Triple where
+
+
+import Language.Haskell.Liquid.ProofCombinators
+
+import GenericProofs.Iso
+import GenericProofs.TH
+import GenericProofs.VerifiedMonoid
+import GenericProofs.VerifiedMonoid.Generics
+import GenericProofs.VerifiedMonoid.Instances
+
+import Generics.Deriving.Newtypeless.Base.Internal
+
+
+{-@ data Triple a b c = MkTriple { fld1 :: a, fld2 :: b, fld3 :: c } @-}
+data Triple a b c = MkTriple a b c
+
+{-@ axiomatize fromTriple @-}
+{-@ axiomatize toTriple @-}
+{-@ tofTriple :: a:Triple a b c
+              -> { toTriple (fromTriple a) == a }
+@-}
+{-@ fotTriple :: a:RepTriple a b c x
+              -> { fromTriple (toTriple a) == a }
+@-}
+$(deriveIso "RepTriple"
+            "toTriple" "fromTriple"
+            "tofTriple" "fotTriple"
+            "isoTriple"
+            ''Triple)
+
+vmonoidTriple :: VerifiedMonoid (Triple Unit Unit Unit)
+vmonoidTriple =
+    vmonoidIso (isoSym isoTriple)
+               $ vmonoidM1
+               $ vmonoidM1
+               $ vmonoidProd (vmonoidM1 $ vmonoidK1 vmonoidUnit)
+               $ vmonoidProd (vmonoidM1 $ vmonoidK1 vmonoidUnit)
+                         (vmonoidM1 $ vmonoidK1 vmonoidUnit)
+
