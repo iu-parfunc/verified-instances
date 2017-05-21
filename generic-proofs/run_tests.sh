e84d9184d48222de8274559a1af6fd9af8a1c0e2
Ryan Newton <rrnewton@gmail.com>
2017-05-20 23:31:23 -0400

    Add example where there are TWO datatypes, with one using the other.

--- generic-proofs/run_tests.sh
+++ generic-proofs/run_tests.sh
@@ -5,7 +5,7 @@ stack install
 
 run="stack exec liquid -- -iinclude -isrc "
 
-for file in List Newtype Product Product3 Sum Unit ;
+for file in List Newtype Product Product3 Sum Unit TwoData ;
 do
     $run src/GenericProofs/VerifiedEq/Examples/${file}.hs
 done
