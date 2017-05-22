cbdbae2d9ec1989bbec3db658064072f99477fb0
Vikraman Choudhury <git@vikraman.org>
2017-05-21 22:56:04 -0400

    Iso version of These

---
+++ generic-proofs/src/GenericProofs/VerifiedOrd/Examples/These.hs
@@ -0,0 +1,55 @@
+{-@ LIQUID "--higherorder"        @-}
+{-@ LIQUID "--totality"           @-}
+{-@ LIQUID "--exactdc"            @-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
+module GenericProofs.VerifiedOrd.Examples.These
+  ( These(..)
+  , RepThese
+  , toThese
+  , fromThese
+  , tofThese
+  , fotThese
+  , isoThese
+  , vordThese
+  ) where
+
+import Language.Haskell.Liquid.ProofCombinators
+
+import GenericProofs.Iso
+import GenericProofs.TH
+import GenericProofs.VerifiedOrd
+import GenericProofs.VerifiedOrd.Generics
+import GenericProofs.VerifiedOrd.Instances
+
+import Generics.Deriving.Newtypeless.Base.Internal
+
+import Prelude                                     hiding (These (..))
+
+{-@ data These = This a | That b | These { a :: a, b :: b } @-}
+data These a b = This a | That b | These a b deriving (Eq)
+
+{-@ axiomatize fromThese @-}
+{-@ axiomatize toThese @-}
+{-@ tofThese :: a:These a b
+             -> { toThese (fromThese a) == a }
+@-}
+{-@ fotThese :: a:RepThese a b x
+             -> { fromThese (toThese a) == a }
+@-}
+$(deriveIso "RepThese"
+            "toThese" "fromThese"
+            "tofThese" "fotThese"
+            "isoThese"
+            ''These)
+
+vordThese :: (Eq a, Eq b) => VerifiedOrd a -> VerifiedOrd b -> VerifiedOrd (These a b)
+vordThese vorda vordb =
+    vordIso (isoSym isoThese) $
+    vordM1 $
+    vordSum
+        (vordM1 $ vordM1 $ vordK1 vorda)
+        (vordSum
+             (vordM1 $ vordM1 $ vordK1 vordb)
+             (vordM1 $ vordProd (vordM1 $ vordK1 vorda)
+                                (vordM1 $ vordK1 vordb)))
