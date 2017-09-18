f0ca1ef46a0fc793bfc89fc11fc24ed4953285a7
Ryan Newton <rrnewton@gmail.com>
2017-09-18 16:47:21 -0400

    oops, silly typo in that script

--- experiments/coq/build_with_docker.sh
+++ experiments/coq/build_with_docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Uses docker but MUTATES the current directory! Naughty.
+# Brings up coqide.
+
 set -xe
 
 TAG=verified-instances
@@ -7,10 +10,9 @@ TAG=verified-instances
 docker build -t $TAG .
 
 function go () {
-    docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/coq $TAG -w /coq $*
+    docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/coq -w /coq $TAG $*
 }
 
 go make clean
 go make
-
-# coq coqide /coq/LemmasV2.v
+go coqide LemmasV2.v
