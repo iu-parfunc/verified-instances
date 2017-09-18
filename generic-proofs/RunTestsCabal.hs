f28af9f4dbc0abffe636227b03d18ecdf7c4ca7c
Ryan Scott <ryan.gl.scott@gmail.com>
2017-09-18 10:58:25 -0400

    Bash scripts are silly anyway

---
+++ generic-proofs/RunTestsCabal.hs
@@ -0,0 +1,55 @@
+module Main (main) where
+
+import Control.Exception
+import Data.Foldable
+import System.Directory
+import System.Environment
+import System.Exit
+import System.FilePath
+import System.Process
+
+main :: IO ()
+main = do
+  mbCabalHead <- lookupEnv "CABAL_HEAD"
+  let cabalHead = case mbCabalHead of
+                    Just path -> path
+                    Nothing   -> "cabal"
+  hsFiles <- filter ((== ".hs") . takeExtension) <$> getDirectoryContentsRecursive "src"
+  putStrLn "Preparing to run liquid on:"
+  forM_ hsFiles $ \hsFile -> putStrLn $ '\t':hsFile
+  forM_ hsFiles $ \hsFile -> do
+    let cmd = cabalHead ++ " new-run liquid -- -iinclude -isrc src" </> hsFile
+    putStrLn cmd
+    (_, _, _, handle) <- createProcess $ shell cmd
+    ex <- waitForProcess handle
+    case ex of
+      ExitSuccess   -> return ()
+      ExitFailure{} -> throw ex
+
+getDirectoryContentsRecursive :: FilePath -> IO [FilePath]
+getDirectoryContentsRecursive dir0 =
+  fmap tail (recurseDirectories dir0 [""])
+
+recurseDirectories :: FilePath -> [FilePath] -> IO [FilePath]
+recurseDirectories _    []         = return []
+recurseDirectories base (dir:dirs) = do
+  (files, dirs') <- collect [] [] =<< getDirectoryContents (base </> dir)
+
+  files' <- recurseDirectories base (dirs' ++ dirs)
+  return (dir : files ++ files')
+
+  where
+    collect files dirs' []              = return (reverse files, reverse dirs')
+    collect files dirs' (entry:entries) | ignore entry
+                                        = collect files dirs' entries
+    collect files dirs' (entry:entries) = do
+      let dirEntry  = dir </> entry
+          dirEntry' = addTrailingPathSeparator dirEntry
+      isDirectory <- doesDirectoryExist (base </> dirEntry)
+      if isDirectory
+        then collect files (dirEntry':dirs') entries
+        else collect (dirEntry:files) dirs' entries
+
+    ignore ['.']      = True
+    ignore ['.', '.'] = True
+    ignore _ = False
