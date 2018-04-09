3bbb20bf8c10ccfd5ae09c02cdd5fa95f868f903
Vikraman Choudhury <git@vikraman.org>
2018-04-09 16:11:58 -0400

    Add an interpreter for Trace

--- experiments/coq/Heap2.v
+++ experiments/coq/Heap2.v
@@ -4,10 +4,11 @@ Require Import Coq.Logic.FunctionalExtensionality.
 
 Require Import Val.
 Require Import IVar.
+Require Import Trace.
 
 Module Type Heap.
   Parameter Heap : Type -> Type.
-  Axiom newHeap : forall {A}, Heap A.
+  Axiom newHeap : forall {A}, (IVar A -> Heap A) -> Heap A.
   Axiom writeHeap : forall {A}, IVar A -> A -> Heap A -> Heap A.
   Axiom readHeap : forall {A}, IVar A -> Heap A -> A.
   Axiom splitHeap : forall {A}, Heap A -> Heap A * Heap A.
@@ -20,13 +21,16 @@ End Heap.
 Module Par (H : Heap).
   Import H.
 
-  Definition get {A} (i : IVar A) (k : A -> Heap A -> unit) (h : Heap A) : unit :=
+  Definition new {A} (k : IVar A -> Heap A) : Heap A :=
+    newHeap k.
+
+  Definition get {A} (i : IVar A) (k : A -> Heap A -> Heap A) (h : Heap A) : Heap A :=
     k (readHeap i h) h.
 
-  Definition put {A} (i : IVar A) (v : A) (k : Heap A -> unit) (h : Heap A) : unit :=
+  Definition put {A} (i : IVar A) (v : A) (k : Heap A -> Heap A) (h : Heap A) : Heap A :=
     k (writeHeap i v h).
 
-  Lemma putsCommute {A} (i1 i2 : IVar A) (v1 v2 : A) (k : Heap A -> unit) :
+  Lemma putsCommute {A} (i1 i2 : IVar A) (v1 v2 : A) (k : Heap A -> Heap A) :
     put i2 v2 (fun h => put i1 v1 k h) = put i1 v1 (fun h => put i2 v2 k h).
   Proof.
     compute.
@@ -35,4 +39,17 @@ Module Par (H : Heap).
     apply (commutesHeap h (writeHeap i2 v2) (writeHeap i1 v1)).
     congruence.
   Qed.
+
+  Definition fork {A} (k1 k2 : Heap A -> Heap A) (h : Heap A) : Heap A :=
+    let (h1, h2) := splitHeap h in joinHeap (k1 h) (k2 h).
+
+  Fixpoint runTrace (tr : Trace) (h : Heap Val) : Heap Val :=
+    match tr with
+    | Get i k => get i (fun v h' => runTrace (k v) h') h
+    | Put i v tr1 => put i v (fun h' => runTrace tr1 h') h
+    | New k => new (fun i => runTrace (k i) h)
+    | Fork tr1 tr2 => fork (runTrace tr1) (runTrace tr2) h
+    | Done => h
+    end.
+
 End Par.
